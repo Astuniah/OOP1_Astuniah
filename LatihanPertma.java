b2434315072b20318599cb13bf7335c3fd3f1041
Astuniah <asthu02@gmail.com>
2016-03-29 20:19:51 +0700

    Added files via upload

---
+++ LatihanPertma.java
@@ -0,0 +1,90 @@
+package latihanpertma;
+
+public class LatihanPertma {
+
+  public static void main(String[] args) {
+String nm = "As ";
+String nm2 = "Astuniah";
+System.out.println(nm + nm2);
+String nm3 = "Pusakanagara ";
+String nm4 = "Subang";
+System.out.println (nm3 + nm4);
+String nmA, nmB, tlhr, tglLhr, blnLhr, thnLhr;
+nmA ="As";
+nmB ="Astuniah";
+tlhr ="Subang";
+tglLhr = " 19 ";
+blnLhr ="juni";
+        thnLhr ="1994";
+       
+ 
+System.out.println("Nama Lengkap : " + nmA + " " + nmB);
+        System.out.println("Tempat Lahir : " +tlhr);
+        System.out.println("Tanggal Lahir :" + tglLhr);
+        System.out.println("Bulan Lahir :" + blnLhr);
+        System.out.println("Tahun Lahir :" + thnLhr);
+        String intA = "4";
+        String intB = "6";
+        System.out.println("penjumlahan" + intA + "+" + intB );
+        System.out.println("1");
+        System.out.println("2");
+         System.out.println("3");
+        System.out.println("4");
+         System.out.println("5");
+        System.out.println("6");
+         System.out.println("7");
+        System.out.println("8");
+         System.out.println("9");
+        System.out.println("10");
+         System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+         System.out.println("15");
+        System.out.println("16");
+         System.out.println("17");
+        System.out.println("18");
+         System.out.println("19");
+        System.out.println("20");
+         System.out.println("21");
+        System.out.println("22");
+         System.out.println("23");
+        System.out.println("24");
+        System.out.println("25");
+        System.out.println("26");
+         System.out.println("27");
+        System.out.println("28");
+         System.out.println("29");
+        System.out.println("30");
+         System.out.println("31");
+        System.out.println("32");
+         System.out.println("33");
+        System.out.println("34");
+         System.out.println("35");
+         System.out.println("36");
+        System.out.println("37");
+         System.out.println("38");
+        System.out.println("39");
+         System.out.println("40");
+        System.out.println("41");
+         System.out.println("42");
+        System.out.println("43");
+         System.out.println("44");
+         System.out.println("44");
+        System.out.println("45");
+         System.out.println("46");
+        System.out.println("47");
+         System.out.println("48");
+        System.out.println("49");
+         System.out.println("50");
+         for (int i =51; i <=100; i++);
+        
+
+      System.out.println("Hello world");
+System.out.println("hy bandung");
+System.out.println("semangat kawan");
+System.out.print("berlatih terus");
+System.out.println("Astuniah");
+System.out.println("latihan 'terus' pemograman");
+    }
+    }
