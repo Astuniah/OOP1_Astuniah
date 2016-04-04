a2531bb8bd3b7a3f84c21df35baa720e5e2947c1
Astuniah <asthu02@gmail.com>
2016-04-04 14:39:10 +0700

    Added files via upload

---
+++ ArrayJava.java
@@ -0,0 +1,17 @@
+
+package array.java;
+
+public class ArrayJava {
+
+    public static void main(String[] args) {
+    System.out.println("DAFTAR PRODUK TOKO ONLINE");
+    String [] x  = {"Baju","jeans","Sepatu","Jam tangan","bhoneka","tas"};
+     System.out.println("1." + x [0]);
+        System.out.println("2." + x [1]);
+        System.out.println("3." +  x [2]);
+        System.out.println("4." + x [3]);
+        System.out.println("5." +  x [4]);
+        System.out.println("6." + x [5]);
+    }
+    
+}
